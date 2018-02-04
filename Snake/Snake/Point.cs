2f4e8f77ffe1f76f4d496b504c61bf33b2581b9e
Aleksandra <alex.suslova2011@yandex.ru>
2018-02-04 16:42:10 +0400

    Принцип инкапсуляции

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -12,6 +12,20 @@ namespace Snake
         public int y;
         public char sym;
 
+        /// <summary>
+        /// Конструктор
+        /// Принцип инкапсуляции
+        /// </summary>
+        /// <param name="_x">координата х</param>
+        /// <param name="_y">координата у</param>
+        /// <param name="_sym">символ точки</param>
+        public Point(int _x, int _y, char _sym)   
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
